ba2c3f4b44158bb3099be181e461a68a26ee9639
leandrocgsi <leandrocgsi@gmail.com>
2021-08-14 10:01:48 -0300

    Adding API Server

---
+++ server/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IBookBusiness.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Utils;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        PagedSearchVO<BookVO> FindWithPagedSearch(
+            string title, string sortDirection, int pageSize, int page);
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
---
+++ server/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,85 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Implementations;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Utils;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+
+        private readonly IRepository<Book> _repository;
+
+        private readonly BookConverter _converter;
+
+        public BookBusinessImplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+            _converter = new BookConverter();
+        }
+
+        // Method responsible for returning all people,
+        public List<BookVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public PagedSearchVO<BookVO> FindWithPagedSearch(
+            string title, string sortDirection, int pageSize, int page)
+        {
+            var sort = (!string.IsNullOrWhiteSpace(sortDirection)) && !sortDirection.Equals("desc") ? "asc" : "desc";
+            var size = (pageSize < 1) ? 10 : pageSize;
+            var offset = page > 0 ? (page - 1) * size : 0;
+
+            string query = @"select * from books p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(title)) query = query + $" and p.title like '%{title}%' ";
+            query += $" order by p.title {sort} limit {size} offset {offset}";
+
+            string countQuery = @"select count(*) from books p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(title)) countQuery = countQuery + $" and p.title like '%{title}%' ";
+
+            var books = _repository.FindWithPagedSearch(query);
+            int totalResults = _repository.GetCount(countQuery);
+
+            return new PagedSearchVO<BookVO>
+            {
+                CurrentPage = page,
+                List = _converter.Parse(books),
+                PageSize = size,
+                SortDirections = sort,
+                TotalResults = totalResults
+            };
+        }
+
+        // Method responsible for returning one book by ID
+        public BookVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        // Method responsible to crete one new book
+        public BookVO Create(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        // Method responsible for updating one book
+        public BookVO Update(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Update(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        // Method responsible for deleting a book from an ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
+
