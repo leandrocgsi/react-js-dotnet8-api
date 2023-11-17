83e0de55a20a6ac9bf56576e8b476accb7e07643
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 20:16:03 -0300

    Migrating from ASP.NET Core 5 to ASP.NET 8

---
+++ server/RestWithASPNETErudio/RestWithASPNETErudio/Business/IBookBusiness.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETErudio.Data.VO;
+using RestWithASPNETErudio.Hypermedia.Utils;
+
+namespace RestWithASPNETErudio.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        PagedSearchVO<BookVO> FindWithPagedSearch(
+            string title, string sortDirection, int pageSize, int page);
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
